3388fe38079832bedead7658287190e08ee1df55
cuppajoeman <ccn@cuppajoeman.com>
2024-10-09 14:44:54 -0400

    chore: use search as submodule

--- search/search_callbacks.js
+++
@@ -1,5 +0,0 @@
-// Function to handle opening the selected item
-function on_select_callback(item) {
-    console.log("Opened:", item.textContent);  // Placeholder callback for when Enter is pressed
-    window.location.href = '/' + item.textContent
-}
--- search/search_list.js
+++
@@ -1,2 +0,0 @@
-// Mock data: array of numbers from 1 to 100 as strings
-var data = Array.from({length: 100}, (_, i) => (i + 1).toString());
